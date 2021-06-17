aa4a906cd2f2ec67ee1f08732c09224c9119b24f
yodyindra <indrayody04@gmail.com>
2021-06-17 21:18:50 +0800

    menambahkan connection

---
+++ connection.php
@@ -0,0 +1,24 @@
+<?php
+//error_reporting(E_ALL); ini_set('display_errors', 1);
+//mysqli_report(MYSQLI_REPORT_ERROR);
+
+$host = "localhost";
+$user = "root";
+$pass = "";
+$database = "db_mhs";
+
+// Create connection
+
+$db = mysqli_connect($host, $user, $pass, $database);
+
+if( !$db ){
+    die("Gagal terhubung dengan database: " . mysqli_connect_error());
+}
+
+$conn = new mysqli($host, $user, $pass, $database);
+ 
+// Check connection
+if ($conn->connect_error) {
+ die("Connection failed: " . $conn->connect_error);
+}
+?>
